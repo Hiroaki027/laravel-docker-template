6ca361290dbab5906c8f1be1368c6d2b4d2b6475
Hiroaki027 <morning.star.9867@gmail.com>
2024-10-19 21:18:18 +0900

    feat:TodoController Todoモデルをインスタンス化

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -4,10 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Todo;
+
 class TodoController extends Controller
 {
     public function index()
     {
-        return view('todo.index'); //dd関数はLaravelのヘルパ関数であり、デバッグで用いる　
+        $todo = new Todo();
+        $todos = $todo->all();
+        dd($todos);
+
+        return view('todo.index'); //dd関数はLaravelのヘルパ関数であり、デバッグで用いる
     }                             //view関数はview('フォルダ名.ファイル名')
 }
