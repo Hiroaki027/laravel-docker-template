e5ee9a51cca86ea08344a1646b974d015eb0a60d
Hiroaki027 <morning.star.9867@gmail.com>
2024-10-19 22:31:21 +0900

    add: create.blade.php 新規作成画面の追加、及び付随するファイルに記述追加

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -15,4 +15,9 @@ class TodoController extends Controller
 
         return view('todo.index', ['todos' => $todos]);
     }                             //view関数はview('フォルダ名.ファイル名')dd関数はLaravelのヘルパ関数であり、デバッグで用いる
+
+    public function create()
+    {
+        return view('todo.create');
+    }
 }
