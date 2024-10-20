d6b9e98b6936af0c0f49c16dc6fa915761ec2d5e
Hiroaki027 <morning.star.9867@gmail.com>
2024-10-20 19:30:42 +0900

    change:Todo.php 既存の削除機能を論理削除へ変更

--- src/app/Todo.php
+++ src/app/Todo.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Todo extends Model
 {
+    use SoftDeletes;
+
     protected $table = 'todos'; //$tableプロパティ　Eloquementモデルのプロパティの1つ　関連付けるターブル名を指定
 
     protected $fillable = [ //$fillableで許可する項目を記載
